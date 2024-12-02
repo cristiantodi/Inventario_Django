57639215b05a445780c48ad2c1d4d68ea916af67
Cristian Tole <toleko89@hotmail.com>
2024-12-02 17:36:21 -0500

    :running: terminando css Crear Credito

--- abono/models.py
+++ abono/models.py
@@ -9,6 +9,9 @@ class Metodo_pago(models.Model):
 
     def __str__(self):
         return self.nombre_Pago
+    
+# class pago(models.Model):
+
 
 class Abono(models.Model):
     nombre          = models.CharField(max_length=70)
--- abono/urls.py
+++ abono/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.abonos, name="Abonos"),
-    path('crearForm/', views.crearAbono, name='crearForm'),
+    path('crearCreadito/', views.crearAbono, name='crearCreadito'),
     # path('buscar-nombre/', views.buscar_nombre, name='buscar_nombre'),
     path('get-nombre/', views.get_nombre, name='get_nombre'),
 ]
